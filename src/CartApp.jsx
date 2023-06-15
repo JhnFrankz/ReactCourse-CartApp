b245f7bbadc64dfe8d0f98a957432406fb78c8cf
Jhan Frank <jhnfrankz@gmail.com>
2023-06-15 13:32:23 -0500

    Routes with React Router

--- src/CartApp.jsx
+++ src/CartApp.jsx
@@ -1,3 +1,4 @@
+import { Navigate, Route, Routes } from "react-router-dom";
 import { CartView } from "./components/CartView";
 import { CatalogView } from "./components/CatalogView";
 import { useItemsCart } from "./hooks/useItemsCart";
@@ -11,16 +12,30 @@ export const CartApp = () => {
             <div className="container my-4">
 
                 <h3>Cart App</h3>
-                <CatalogView handler={handlerAddProductCart} />
+                <Routes>
+                    <Route
+                        path="catalog"
+                        element={<CatalogView handler={handlerAddProductCart} />}
+                    />
+                    <Route
+                        path="cart"
+                        element={(
+                            // los parentesis del elemento son para indicar que se va a ejecutar una expresión de JS y no un componente
+                            //si la condición es falsa, se muestra el componente CartView
+                            // ? verifica si una variable es diferente de null o undefined
+                            cartItems?.length <= 0 ?
+                                <div className="alert alert-warning">No hay productos en el carrito de compras</div>
+                                :
+                                (
+                                    <div className="my-4 w-50">
+                                        <CartView items={cartItems} handlerDelete={handlerDeleteProductCart} />
+                                    </div>
+                                )
+                        )}
+                    />
+                    <Route path="/" element={<Navigate to={'/catalog'} />} />
+                </Routes>
 
-                { //si la condición es falsa, se muestra el componente CartView
-                    // ? verifica si una variable es diferente de null o undefined
-                    cartItems?.length <= 0 || (
-                        <div className="my-4 w-50">
-                            <CartView items={cartItems} handlerDelete={handlerDeleteProductCart} />
-                        </div>
-                    )
-                }
             </div>
         </>
     );
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { CartApp } from './CartApp'
+import { BrowserRouter } from 'react-router-dom'
 
+// BrowserRouter funcionará con un sistema de rutas
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <CartApp />
+    <BrowserRouter>
+      <CartApp />
+    </BrowserRouter>
   </React.StrictMode>,
 )
