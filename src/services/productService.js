4188cec751d202724b08c30664b243447ca28868
Jhan Frank <jhnfrankz@gmail.com>
2023-06-25 09:49:06 -0500

    Consuming backend REST API - modifying productService

--- src/services/productService.js
+++ src/services/productService.js
@@ -1,6 +1,10 @@
-import { products } from "../data/products";
 
-export const getProducts = () => {
+export const getProducts = async () => {
+
+    // await espera a que se resuelva la promesa y luego devuelve el response y no la promesa
+    // las promesas tienen un tiempo de vida, si no se resuelven en ese tiempo, se rechazan
+    const response = await fetch('http://localhost:8080/products');
+    const products = await response.json();
     return products;
 };
 
